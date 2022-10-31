1bc975bccc2a3914dee012d11dfc9feefda68527
Calpurniax <sofia.gracia@gmail.com>
2022-10-31 09:54:22 +0100

    fix bad guys

--- js/main.js
+++ js/main.js
@@ -7,12 +7,23 @@ const textResult = document.querySelector('.js-text');
 
 let random = 0;
 
-//función para la facción malvada
+//función para elegir un número entre el 1 y el 6
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+    return Math.ceil(Math.random() * max);    
    }
-
+//función para convertir el valor random en valores entre 2 y 5, como corresponde a las razas malvadas
+function badGuysGenerator (){
+   let random = getRandomNumber(6);
+    if (random <= 2){
+        random =2
+    }else if (random >2 && random<5) {
+        random =3
+    }else if (random >=5){
+        random =5;
+    }
+    return random;
+} 
 //función para la facción bondadosa
 function goodSelect () {    
     const selectValue = parseInt(select.value);     
@@ -41,8 +52,8 @@ function htmlResult (result){
 function handleClick (event) {
     event.preventDefault();
     const goodGuys= goodSelect();    
-    let random = getRandomNumber(6)    
-    const result = compare (goodGuys, random);    
+    const badGuys = badGuysGenerator(); 
+    const result = compare (goodGuys, badGuys);    
 }
 
 btn.addEventListener('click', handleClick);
