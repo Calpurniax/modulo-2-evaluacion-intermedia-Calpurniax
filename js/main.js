ff61b6363401eb5caa7ba607ca1009e47d6a027d
Calpurniax <sofia.gracia@gmail.com>
2022-10-31 09:35:32 +0100

    work in compare function

--- js/main.js
+++ js/main.js
@@ -8,40 +8,40 @@ const textResult = document.querySelector('.js-text');
 let random = 0;
 
 //función para la facción malvada
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    }
-
-//let random = getRandomNumber(6);
-//console.log(random);
 
 //función para la facción bondadosa
 function goodSelect () {    
     const selectValue = parseInt(select.value);     
     return selectValue;
-
 }
 
 //funcion para pintar en el HTML
 
 
 //funcion para comparar las dos facciones
-function compare (selectValue, random){
-    if (selectValue > random){
-        console.log ("ganan los buenos")
+
+function compare (good, evil){
+    if (good > evil){
+        console.log (good,evil);
+        console.log ("ganan los buenos");
+       }else if (good >= evil){
+        console.log (good,evil);
+        console.log ("empate");
        } else {
-        console.log ("ganan los malos")
+        console.log (good,evil);
+        console.log ("ganan los malos");
        }
 }
 
 function handleClick (event) {
     event.preventDefault();
-    const goodGuys= goodSelect();
-    console.log (typeof(goodGuys));
-   /* let random = getRandomNumber(6)
-    goodSelect ();
-    compare ();*/
-   
+    const goodGuys= goodSelect();    
+    let random = getRandomNumber(6)    
+    compare (goodGuys, random);
 }
 
 btn.addEventListener('click', handleClick);
